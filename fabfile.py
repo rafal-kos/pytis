c1798133956ebcd22e3b86624d985302d67e2905
xjonc <none@none>
2011-01-01 20:00:21 +0100

    removed second application from fabric file

--- fabfile.py
+++ fabfile.py
@@ -11,5 +11,5 @@ def deploy():
     run('unzip -o pytis.zip -d /home/dcsdebica/www/pytis/')
     run('unzip -o pytis.zip -d /home/dcsdebica/www/pytis_2/')
     run('restart-app pytis_v1')
-    run('restart-app pytis_v2')
+    #run('restart-app pytis_v2')
     run('rm pytis.zip')
